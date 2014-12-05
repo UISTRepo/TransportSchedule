1db0189515c51716b88ee78a158b4cad9c4b69b4
Naum Tuntev <tuntev@gmail.com>
2014-12-05 11:09:16 +0100

    Changed default vie engine from hjs to ejs (easier partials manipulation)

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {title: 'The Title'});
 });
 
 router.post('/login', function(req, res) {
